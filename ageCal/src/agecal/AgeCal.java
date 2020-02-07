9796ce4698808cccf5cf66d9ccf4d58c6a5dc5a7
Ghostnxc <55505179+Ghostnxc@users.noreply.github.com>
2020-02-07 16:29:04 -0500

    Add files via upload

---
+++ ageCal/src/agecal/AgeCal.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package agecal;
+import java.util.Scanner;
+/**
+ *
+ * @author phant
+ */
+
+import java.util.Scanner;
+public class AgeCal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int ageNine;
+        
+              ageNine = 9;  
+               
+               System.out.println("How old are you:");
+               Scanner inPut = new Scanner(System.in);
+               int age= inPut.nextInt();
+               int diff =  age - ageNine;
+               int sum =  age + ageNine;
+               System.out.println("you are : "+age);
+               System.out.println("you will be  : "+sum+" : in 9 years");
+               System.out.println("you where  : "+diff+" : 9 years ago");
+        // TODO code application logic here
+    }
+    
+}
